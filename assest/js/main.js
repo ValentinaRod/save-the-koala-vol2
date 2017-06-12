b767ef4ec28505a3e3f78897503404198edbc0fc
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-11 20:32:05 -0400

    boostrap seccion 6

--- assest/js/main.js
+++ assest/js/main.js
@@ -7,6 +7,15 @@ $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
 
+$('#myModal2').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#myModal3').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+
 /*carusel*/
 
 $('#myCollapsible').on('hidden.bs.collapse', function () {
